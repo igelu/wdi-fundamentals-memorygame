3312d53afb5590f183c44f81a7e8bd69dea147f5
igelu <pinktofu@gmail.com>
2017-12-29 18:58:39 -0800

    game logic issue

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,17 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[2]) {
-   alert("You found a match!");
-} else {
-  alert("Sorry, try again.");
-}
+var checkForMatch = function() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
+};
+var flipCard = function(cardId) {
+  flipCard(0);
+  //I assume this is where the issue is, just dont know how to fix
+  flipCard(2);
+  console.log("User flipped" + " " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
