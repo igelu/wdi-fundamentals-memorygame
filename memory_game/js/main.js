5644a7c668379eeac910f46fdb8b6a2f21466f6a
igelu <pinktofu@gmail.com>
2017-12-29 19:56:43 -0800

    fixed game logic unit 9

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,16 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 var checkForMatch = function() {
-if (cardsInPlay[0] === cardsInPlay[1]) {
+  if (cardsInPlay[0] === cardsInPlay[1]){
     console.log("You found a match!");
   } else {
     console.log("Sorry, try again.");
   }
 };
 var flipCard = function(cardId) {
-  flipCard(0);
-  //I assume this is where the issue is, just dont know how to fix
-  flipCard(2);
   console.log("User flipped" + " " + cards[cardId]);
   cardsInPlay.push(cards[cardId]);
   checkForMatch();
 };
+flipCard(0);
+flipCard(2);
